4c14c22fcf46fa1085acbbbddfc05303ba754f1d
Wade Jakahi <wjakahi@gmail.com>
2018-09-26 11:58:57 -0400

    init

---
+++ precache-manifest.7734b1e0b4e5be7da25706abd4f88103.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "0028fef895d185fa8c83",
+    "url": "/js/chunk-vendors.794a3f60.js"
+  },
+  {
+    "revision": "ed707fae5f8fec33fb54",
+    "url": "/js/app.9182f460.js"
+  },
+  {
+    "revision": "55c3099d5cb7fefc6f45",
+    "url": "/js/about.28f21103.js"
+  },
+  {
+    "revision": "c7b2a0aed58731c8519be88900dae373",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "0028fef895d185fa8c83",
+    "url": "/css/chunk-vendors.ae226d06.css"
+  },
+  {
+    "revision": "ed707fae5f8fec33fb54",
+    "url": "/css/app.c6675b74.css"
+  },
+  {
+    "revision": "55c3099d5cb7fefc6f45",
+    "url": "/css/about.3d85339c.css"
+  }
+];

d9e119a12d5d1beb816ee6fd6ea13600b94eb669
Wade Jakahi <wjakahi@gmail.com>
2019-06-06 08:18:23 -0400

    fixing clearing of pasted value

--- precache-manifest.d2dbf04cec0a1e7867b5daaf7c66a207.js
+++ precache-manifest.d2dbf04cec0a1e7867b5daaf7c66a207.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
-    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.0509ab09.woff2"
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.96c47680.eot"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/chs.compliance.client/robots.txt"
   },
   {
-    "revision": "96c476804d7a788cc1c05351b287ee41",
-    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.96c47680.eot"
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.0509ab09.woff2"
   },
   {
-    "revision": "d25c8a7ac4d61c79eba2",
+    "revision": "f547f10f9ef2f732d928",
     "url": "/chs.compliance.client/css/app.ad62faa7.css"
   },
   {
@@ -24,12 +24,12 @@ self.__precacheManifest = [
     "url": "/chs.compliance.client/js/chunk-vendors.a37b9e66.js"
   },
   {
-    "revision": "b313497d720dd703a65d",
-    "url": "/chs.compliance.client/js/about.ca7b8e74.js"
+    "revision": "14062804445f279423e2",
+    "url": "/chs.compliance.client/js/about.00dfb54b.js"
   },
   {
-    "revision": "d25c8a7ac4d61c79eba2",
-    "url": "/chs.compliance.client/js/app.66f40a0c.js"
+    "revision": "f547f10f9ef2f732d928",
+    "url": "/chs.compliance.client/js/app.66ab2312.js"
   },
   {
     "revision": "29b882f018fa6fe75fd338aaae6235b8",
@@ -40,7 +40,7 @@ self.__precacheManifest = [
     "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
   },
   {
-    "revision": "be3c42e2abe372121c6bd0ae7f842096",
+    "revision": "9435cd2d4d4e5a1652aca9491d003954",
     "url": "/chs.compliance.client/index.html"
   },
   {
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/chs.compliance.client/img/chsli-logo.jpg"
   },
   {
-    "revision": "b313497d720dd703a65d",
+    "revision": "14062804445f279423e2",
     "url": "/chs.compliance.client/css/about.f10c3c18.css"
   }
 ];

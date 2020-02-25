f50161832f5f165b8884274ec5a4a0f337b74e15
Wade Jakahi <wjakahi@gmail.com>
2020-02-25 12:07:02 -0500

    2-25 fixes

---
+++ precache-manifest.5274964e84cd1d0b9085efe73d96df8a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/chs.compliance.client/robots.txt"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "078f1807d70472f89e79",
+    "url": "/chs.compliance.client/css/app.ad62faa7.css"
+  },
+  {
+    "revision": "4a239aad0f792c74dce8",
+    "url": "/chs.compliance.client/css/chunk-vendors.9508a8c8.css"
+  },
+  {
+    "revision": "4a239aad0f792c74dce8",
+    "url": "/chs.compliance.client/js/chunk-vendors.a37b9e66.js"
+  },
+  {
+    "revision": "e73443f5e6c447aa0268",
+    "url": "/chs.compliance.client/js/about.b0b479ec.js"
+  },
+  {
+    "revision": "078f1807d70472f89e79",
+    "url": "/chs.compliance.client/js/app.8e844f1a.js"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "50418a7b8005ec4ca6e9c0a74d1ea029",
+    "url": "/chs.compliance.client/index.html"
+  },
+  {
+    "revision": "6b736556943b551d5516e4128c72d2cb",
+    "url": "/chs.compliance.client/img/chsli-logo.jpg"
+  },
+  {
+    "revision": "e73443f5e6c447aa0268",
+    "url": "/chs.compliance.client/css/about.c1472296.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chs.compliance.client/precache-manifest.2dc5702301b74fc40bf4064d1ce31feb.js"
+  "/chs.compliance.client/precache-manifest.5274964e84cd1d0b9085efe73d96df8a.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "chsli.compliance.client"});

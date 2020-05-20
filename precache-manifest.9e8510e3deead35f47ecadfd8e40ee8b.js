8881604a0d14e7012bcf122b72d8958e93b6deac
Wade Jakahi <wjakahi@gmail.com>
2020-05-20 14:43:56 -0400

    build for production

--- precache-manifest.9e8510e3deead35f47ecadfd8e40ee8b.js
+++ precache-manifest.9e8510e3deead35f47ecadfd8e40ee8b.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
-    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.0509ab09.woff2"
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.96c47680.eot"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/chs.compliance.client/robots.txt"
   },
   {
-    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
-    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.29b882f0.woff"
   },
   {
-    "revision": "679c29d4544506c261e6",
+    "revision": "1fd21e5fb75c608dbb39",
     "url": "/chs.compliance.client/css/app.ad62faa7.css"
   },
   {
@@ -28,19 +28,19 @@ self.__precacheManifest = [
     "url": "/chs.compliance.client/js/about.b8b55ecf.js"
   },
   {
-    "revision": "679c29d4544506c261e6",
-    "url": "/chs.compliance.client/js/app.e9111e82.js"
+    "revision": "1fd21e5fb75c608dbb39",
+    "url": "/chs.compliance.client/js/app.664d4206.js"
   },
   {
-    "revision": "96c476804d7a788cc1c05351b287ee41",
-    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.96c47680.eot"
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.0509ab09.woff2"
   },
   {
-    "revision": "29b882f018fa6fe75fd338aaae6235b8",
-    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.29b882f0.woff"
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/chs.compliance.client/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
   },
   {
-    "revision": "07900113bcdb09bf99ab6e6be91cb0ec",
+    "revision": "23a6b58aeb38d9a5bfbcad71e6732bed",
     "url": "/chs.compliance.client/index.html"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/chs.compliance.client/precache-manifest.21479c5461687ebe2f74c779cc052f4e.js"
+  "/chs.compliance.client/precache-manifest.9e8510e3deead35f47ecadfd8e40ee8b.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "chsli.compliance.client"});

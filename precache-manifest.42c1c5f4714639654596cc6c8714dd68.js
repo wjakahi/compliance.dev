ccad9b4c9395ac5a76bb283fa6af8bb29fc923ac
Wade Jakahi <wjakahi@gmail.com>
2020-05-21 12:22:09 -0400

    fixed education blob display issue

--- precache-manifest.42c1c5f4714639654596cc6c8714dd68.js
+++ precache-manifest.42c1c5f4714639654596cc6c8714dd68.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
-    "url": "/compliance.prod/fonts/MaterialIcons-Regular.0509ab09.woff2"
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/compliance.prod/fonts/MaterialIcons-Regular.96c47680.eot"
   },
   {
     "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
     "url": "/compliance.prod/robots.txt"
   },
   {
-    "revision": "29b882f018fa6fe75fd338aaae6235b8",
-    "url": "/compliance.prod/fonts/MaterialIcons-Regular.29b882f0.woff"
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/compliance.prod/fonts/MaterialIcons-Regular.0509ab09.woff2"
   },
   {
-    "revision": "844a445b174d45797236",
+    "revision": "c11df960dba2b93f760b",
     "url": "/compliance.prod/css/app.ad62faa7.css"
   },
   {
@@ -24,23 +24,23 @@ self.__precacheManifest = [
     "url": "/compliance.prod/js/chunk-vendors.a37b9e66.js"
   },
   {
-    "revision": "ddd70491471e6cbed80a",
-    "url": "/compliance.prod/js/about.b8b55ecf.js"
+    "revision": "4f242d6bcd724d08b870",
+    "url": "/compliance.prod/js/about.948c89ef.js"
   },
   {
-    "revision": "844a445b174d45797236",
-    "url": "/compliance.prod/js/app.a71c0b2a.js"
+    "revision": "c11df960dba2b93f760b",
+    "url": "/compliance.prod/js/app.8469677d.js"
   },
   {
-    "revision": "96c476804d7a788cc1c05351b287ee41",
-    "url": "/compliance.prod/fonts/MaterialIcons-Regular.96c47680.eot"
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/compliance.prod/fonts/MaterialIcons-Regular.29b882f0.woff"
   },
   {
     "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
     "url": "/compliance.prod/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
   },
   {
-    "revision": "279d589731f8ed98fe4311effc0fd62f",
+    "revision": "e3551ca2903d1297289acc909cb118d1",
     "url": "/compliance.prod/index.html"
   },
   {
@@ -48,7 +48,7 @@ self.__precacheManifest = [
     "url": "/compliance.prod/img/chsli-logo.jpg"
   },
   {
-    "revision": "ddd70491471e6cbed80a",
+    "revision": "4f242d6bcd724d08b870",
     "url": "/compliance.prod/css/about.53a5596a.css"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/compliance.prod/precache-manifest.1d76373c350b8b204c581ccf98438a48.js"
+  "/compliance.prod/precache-manifest.42c1c5f4714639654596cc6c8714dd68.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "chsli.compliance.client"});

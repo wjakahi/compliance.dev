36810b042d59e60949ac6720b0ebb409b2bc61df
Wade Jakahi <wjakahi@gmail.com>
2020-07-24 13:23:21 -0400

    108, 106

--- precache-manifest.536c80bc5157d313190e35bf131dfea8.js
+++ precache-manifest.536c80bc5157d313190e35bf131dfea8.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "3f598cff2eb9834ac4bc",
-    "url": "/compliance.prod/css/about.16856207.css"
+    "revision": "d712042a08eda6fd2e17",
+    "url": "/compliance.prod/css/about.62ec4c93.css"
   },
   {
-    "revision": "3f598cff2eb9834ac4bc",
-    "url": "/compliance.prod/js/about.33f81309.js"
+    "revision": "d712042a08eda6fd2e17",
+    "url": "/compliance.prod/js/about.235eb912.js"
   },
   {
-    "revision": "9590a5d5f6ded0ff7f52",
-    "url": "/compliance.prod/css/app.ad62faa7.css"
+    "revision": "4002aec4b0764af10b15",
+    "url": "/compliance.prod/css/app.554e2a2d.css"
   },
   {
-    "revision": "9590a5d5f6ded0ff7f52",
-    "url": "/compliance.prod/js/app.ac39f3e4.js"
+    "revision": "4002aec4b0764af10b15",
+    "url": "/compliance.prod/js/app.6c1358c4.js"
   },
   {
     "revision": "0b1bbcb943f71d84f179",
@@ -24,23 +24,23 @@ self.__precacheManifest = [
     "url": "/compliance.prod/js/chunk-vendors.3d9da048.js"
   },
   {
-    "revision": "96c476804d7a788cc1c05351b287ee41",
-    "url": "/compliance.prod/fonts/MaterialIcons-Regular.96c47680.eot"
-  },
-  {
     "revision": "0509ab09c1b0d2200a4135803c91d6ce",
     "url": "/compliance.prod/fonts/MaterialIcons-Regular.0509ab09.woff2"
-  },
-  {
-    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
-    "url": "/compliance.prod/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
   },
   {
     "revision": "29b882f018fa6fe75fd338aaae6235b8",
     "url": "/compliance.prod/fonts/MaterialIcons-Regular.29b882f0.woff"
   },
   {
-    "revision": "e2c52d95a4c5f01a8809bd8d16e5479f",
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/compliance.prod/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/compliance.prod/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "3cd038284c23b6d84903f1a73f08ceac",
     "url": "/compliance.prod/index.html"
   },
   {

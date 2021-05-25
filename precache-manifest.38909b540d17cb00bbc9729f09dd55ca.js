90c70d9750c8d274eead08b88bee3119175814b4
Wade Jakahi <wjakahi@gmail.com>
2021-05-25 08:22:10 -0400

    add edu dt to patient selection

--- precache-manifest.38909b540d17cb00bbc9729f09dd55ca.js
+++ precache-manifest.38909b540d17cb00bbc9729f09dd55ca.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "260b7d6989e9e68f1fe4",
+    "revision": "111432906639fe9e3196",
     "url": "/compliance.prod/css/about.ad4787d4.css"
   },
   {
-    "revision": "260b7d6989e9e68f1fe4",
-    "url": "/compliance.prod/js/about.40d26135.js"
+    "revision": "111432906639fe9e3196",
+    "url": "/compliance.prod/js/about.1c8dd6e8.js"
   },
   {
-    "revision": "bb0a1d495f81ea9c7da4",
+    "revision": "7b9221c99f905a5488e5",
     "url": "/compliance.prod/css/app.554e2a2d.css"
   },
   {
-    "revision": "bb0a1d495f81ea9c7da4",
-    "url": "/compliance.prod/js/app.71a6e7d6.js"
+    "revision": "7b9221c99f905a5488e5",
+    "url": "/compliance.prod/js/app.73d3da62.js"
   },
   {
     "revision": "87c388a040b4b7d602ba",
@@ -24,10 +24,6 @@ self.__precacheManifest = [
     "url": "/compliance.prod/js/chunk-vendors.a86c4705.js"
   },
   {
-    "revision": "29b882f018fa6fe75fd338aaae6235b8",
-    "url": "/compliance.prod/fonts/MaterialIcons-Regular.29b882f0.woff"
-  },
-  {
     "revision": "96c476804d7a788cc1c05351b287ee41",
     "url": "/compliance.prod/fonts/MaterialIcons-Regular.96c47680.eot"
   },
@@ -36,11 +32,15 @@ self.__precacheManifest = [
     "url": "/compliance.prod/fonts/MaterialIcons-Regular.0509ab09.woff2"
   },
   {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/compliance.prod/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
     "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
     "url": "/compliance.prod/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
   },
   {
-    "revision": "147592e8b16247fe604fd24cc49be5ab",
+    "revision": "9671c8b71495be9ec182e777fdd605fd",
     "url": "/compliance.prod/index.html"
   },
   {

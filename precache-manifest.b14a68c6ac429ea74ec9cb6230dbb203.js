a3cbf5627c47bfce1c02dc25e8532043995e82dd
Wade Jakahi <wjakahi@gmail.com>
2021-08-06 12:35:26 -0400

    last one

--- precache-manifest.b14a68c6ac429ea74ec9cb6230dbb203.js
+++ precache-manifest.b14a68c6ac429ea74ec9cb6230dbb203.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = [
   {
-    "revision": "4f714332eb7fa8e27ded",
-    "url": "/compliance.dev/css/about.39cb3502.css"
+    "revision": "ad6478821f97910f80fd",
+    "url": "/compliance.dev/css/about.7405834d.css"
   },
   {
-    "revision": "4f714332eb7fa8e27ded",
-    "url": "/compliance.dev/js/about.aa529faa.js"
+    "revision": "ad6478821f97910f80fd",
+    "url": "/compliance.dev/js/about.805859f4.js"
   },
   {
-    "revision": "9a59bd7124a6d52f0b67",
+    "revision": "e1c2cf28fb475a20bce8",
     "url": "/compliance.dev/css/app.554e2a2d.css"
   },
   {
-    "revision": "9a59bd7124a6d52f0b67",
-    "url": "/compliance.dev/js/app.31d5bbc7.js"
+    "revision": "e1c2cf28fb475a20bce8",
+    "url": "/compliance.dev/js/app.0fb391ee.js"
   },
   {
     "revision": "87c388a040b4b7d602ba",
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/compliance.dev/img/logo.34a0d7a9.png"
   },
   {
-    "revision": "29b882f018fa6fe75fd338aaae6235b8",
-    "url": "/compliance.dev/fonts/MaterialIcons-Regular.29b882f0.woff"
-  },
-  {
     "revision": "96c476804d7a788cc1c05351b287ee41",
     "url": "/compliance.dev/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/compliance.dev/fonts/MaterialIcons-Regular.29b882f0.woff"
   },
   {
     "revision": "0509ab09c1b0d2200a4135803c91d6ce",
@@ -44,7 +44,7 @@ self.__precacheManifest = [
     "url": "/compliance.dev/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
   },
   {
-    "revision": "f03ab92eaf6dce822d154328dfd10718",
+    "revision": "647e196e170018b73f9240335f6277bc",
     "url": "/compliance.dev/index.html"
   },
   {
